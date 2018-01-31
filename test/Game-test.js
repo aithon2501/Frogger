9cd51f74c727e5975fcd5878b4b256f61eae8f5e
mike duke <aithon@gmail.com>
2018-01-31 16:53:31 -0700

    Level counter working.
    Game tests started.

---
+++ test/Game-test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const assert = chai.assert;
+const Game = require('../lib/Game');
+
+describe('Game', function() {
+
+    it('should be a function', function () {
+        var game = new Game();
+        assert.isFunction(Game);
+    })
+
+    it('should start at level 1', function () {
+        var game = new Game();
+        assert.notEqual(game.level, 0);
+        assert.equal(game.level, 1);
+    })
+
+    it('should level up', function () {
+        var game = new Game();
+        assert.equal(game.level, 1);
+        game.levelUp();
+        assert.equal(game.level, 2);
+    })
+        
+    it.skip('should detect when two objects collide', function () {
+        var game = new Game();
+
+    })
+})
